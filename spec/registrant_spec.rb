5046684cc2a9e55da5892f2fc49f42cbab05317d
Mark Kendall <mkendall.tutor@gmail.com>
2024-12-12 23:54:27 -0600

    Create registrant_spec.rb skeleton

---
+++ spec/registrant_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+RSpec.describe Registrant do
+  before(:each) do
+    @registrant = Registrant.new()
+  end
+
+  describe '#initialize' do
+    it 'can initialize' do
+      
+    end
+  end
+  
