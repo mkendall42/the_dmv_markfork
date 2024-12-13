6e2b5a1ae54f0041c2e928424e05456a6c66c0fe
Mark Kendall <mkendall.tutor@gmail.com>
2024-12-13 00:05:52 -0600

    Add initialize tests

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -1,13 +1,30 @@
 require 'spec_helper'
+require './lib/registrant.rb'       #May move this to helper file later...
 
 RSpec.describe Registrant do
-  before(:each) do
-    @registrant = Registrant.new()
-  end
-
-  describe '#initialize' do
-    it 'can initialize' do
-      
+    before(:each) do
+        @registrant1 = Registrant.new("Bruce", 18, true, license_data_hash)
+        @registrant2 = Registrant.new("Penny", 15, [false], license_data_hash)
     end
-  end
-  
+
+    describe '#initialize' do
+        it 'can initialize' do
+            default_hash = {:written => false, :license => false, :renewed => false}
+            expect(@registrant).to be_a(Registrant)
+            expect(@registrant.name).to eq("Bruce")
+            expect(@registrant.age).to eq(18)
+            expect(@registrant.permit).to eq(true)
+            expect(@registrant.license_data).to eq(default_hash)
+
+        end
+
+        it 'can handle default paramters correctly on another registrant' do
+            default_hash = {:written => false, :license => false, :renewed => false}
+            expect(@registrant).to be_a(Registrant)
+            expect(@registrant2.name).to eq("Penny")
+            expect(@registrant2.age).to eq(15)
+            expect(@registrant2.permit).to eq(false)
+            expect(@registrant2.license_data).to eq(default_hash)
+        end
+    end
+end
