5d032fe8284aee2a7c6c95c2eaa644260e30aa94
Mark Kendall <mkendall.tutor@gmail.com>
2024-12-13 16:47:58 -0600

    Update Facility initialize() and associated test

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,13 +1,16 @@
 #This clearly tracks a specific DMV facility's core information and serviced rendered (or perhaps I should say """"""services""""""")
 
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :collected_fees, :registered_vehicles
 
   def initialize(facility_info)    #Accepts a hash as argument
     @name = facility_info[:name]
     @address = facility_info[:address]
     @phone = facility_info[:phone]
     @services = []
+    #Added in iteration 2
+    @collected_fees = 0
+    @registered_vehicles = []
   end
 
   def add_service(service)
@@ -18,4 +21,7 @@ class Facility
     #Check to see if this facility provides this service
     @services.find { |service| service == service_specified }
   end
+
+  #Next: registered_vehicles()
+
 end
