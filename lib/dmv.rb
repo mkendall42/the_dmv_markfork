4dcc4b23c2a6be3698b76cdca48c484a56463c45
Mark Kendall <mkendall.tutor@gmail.com>
2024-12-14 19:18:17 -0600

    Update Dmv create_state_facilities() method and associated tests for NY

--- lib/dmv.rb
+++ lib/dmv.rb
@@ -65,10 +65,10 @@ class Dmv
       facilities_incoming_data.each do |facility|
         #Pre-determine name, since this one is nasty, have to concatenate and then 'fix' capitlization the hard way (since each word is capitalized for a title usually)
         name_formatted = facility[:office_name].split.map { |word| word.capitalize }
-        name_formatted << facility[:office_type].split.map { |word| word.capitalize }
-        name_formatted.join(" ")
+        name_formatted << (facility[:office_type].split.map { |word| word.capitalize }).join(" ")
+        name_formatted = name_formatted.join(" ")
 
-        #Fuck this, I'm writing a helper method to just capitalize each word.
+        #Fuck this, I might write a helper method to just capitalize each word.
         #I guess it will live in this class since it's not used anywhere else for this project
         #(a case where OOO is kinda weird / less 'aligned')
         #This could be tricky, though, because it needs to accept an arbitrary number of arguments (maybe pass the hash keys as symbols?).  Don't know how to do this yet...
@@ -76,33 +76,29 @@ class Dmv
         address_formatted << facility[:city].split.map { |word| word.capitalize }
         address_formatted << facility[:state]
         address_formatted << facility[:zip_code]
-        address_formatted.join(" ")
+        address_formatted = address_formatted.join(" ")
 
         #Some of the branches don't seem to have phone numbers...make sure to process 'nil' correctly here
-        if facility[:public_phone_number] != nil
-          
-
-        
-
+        #Also, trying to keep this consistently formatted between states
+        phone = facility[:public_phone_number]
+        if phone != nil
+          phone_formatted = "(" + phone.slice(0, 3) + ") " + phone.slice(3, 3) + "-" + phone.slice(6, 4)
+        end
 
         facility_info = {
-          name: name_formatted
-          # address: "#{facility[:address_li]} #{facility[:address__1]} #{facility[:location]} #{facility[:city]} #{facility[:state]} #{facility[:zip]}",
-          # phone: facility[:phone]
+          name: name_formatted,
+          address: address_formatted,
+          phone: phone_formatted
         }
 
         #Create facility
         new_facility = Facility.new(facility_info)
         add_facility(new_facility)
 
-        #Now add services (to stay with spirit of preexisting codebase - even though they abandoned us!)
-        #Need to convert API's listing of services to our format.  Don't think there's a cleaner way to do this than a use of 'case' or similar:
-        #Can either grab the next 'token' and analyze, or search for specific terms;
-        #I'll go with latter option for now
-        new_facility.add_service("New Drivers License") if facility[:services_p].include?("registration")
-        new_facility.add_service("Renew Drivers License") if facility[:services_p].include?("renew")
-        #NOTE: they don't seem to have granularity in their services regarding tests;
-        #so, I'm going to assume they offer them by default.  CHECK THIS ASSUMPTION
+        #Add services: NOTE - NY office API does NOT return these.
+        #I will assume ALL services are offered (is this ok?)
+        new_facility.add_service("New Drivers License")
+        new_facility.add_service("Renew Drivers License")
         new_facility.add_service("Written Test")
         new_facility.add_service("Road Test")
       end
--- spec/dmv_spec.rb
+++ spec/dmv_spec.rb
@@ -65,16 +65,16 @@ RSpec.describe Dmv do
     end
 
     it 'correctly create facilities array for New York based on API data' do
-      colorado_facilities = DmvDataService.new.ny_dmv_office_locations()
-      @dmv.create_state_facilities("New York", colorado_facilities)
+      newyork_facilities = DmvDataService.new.ny_dmv_office_locations()
+      @dmv.create_state_facilities("New York", newyork_facilities)
 
       #NOTE: this data could change based on the API call.  It should work for the short-term, at least...
       #Don't really know how to make it 'time-proof' in that sense...
-      # expect(@dmv.facilities[0].name).to eq("DMV Tremont Branch")
+      expect(@dmv.facilities[0].name).to eq("Lake Placid County Office")
       #This one was really rough...extra spacing and abbreviations, etc.  Good grief!
-      # expect(@dmv.facilities[1].address).to eq("4685 Peoria Street Suite 101 Arie P. Taylor  Municipal Bldg Denver CO 80239")
-      # expect(@dmv.facilities[2].phone).to eq("(720) 865-4600")
-      # expect(@dmv.facilities[0].services).to eq(["New Drivers License", "Renew Drivers License", "Written Test", "Road Test"])
+      expect(@dmv.facilities[1].address).to eq("560 Warren Street Hudson NY 12534")
+      expect(@dmv.facilities[3].phone).to eq("(718) 966-6155")
+      expect(@dmv.facilities[0].services).to eq(["New Drivers License", "Renew Drivers License", "Written Test", "Road Test"])
     end
   end
 end
